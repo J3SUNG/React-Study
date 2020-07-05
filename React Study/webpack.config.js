2fd08707ba611eb0595b668630741117fe7776b7
J3SUNG <wptjd6141@naver.com>
2020-07-05 21:30:21 +0900

    react 공부

--- React Study/webpack.config.js
+++ React Study/webpack.config.js
@@ -1,3 +1,14 @@
+const path = require('path');
+
 module.exports = {
-    
+   name: 'wordrelay-setting',
+   mode: 'development',
+   devtool: 'eval',
+
+   entry: {
+
+   }, 
+   output: {
+        path: path.join(__dirname, 'dist'),
+   },
 };
