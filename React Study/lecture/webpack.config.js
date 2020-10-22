e15217ec97620a8379bdfb2eddf6bfa9a4910fa5
Jeseong Lee <wptjd6141@naver.com>
2020-10-22 01:57:26 -0700

    파일정리

--- React Study/lecture/webpack.config.js
+++ React Study/lecture/webpack.config.js
@@ -1,30 +1,39 @@
 const path = require('path');
+const { webpack } = require('webpack');
 
 module.exports = {
-    name: 'word-relay-setting',
     mode: 'development',
     devtool: 'eval',
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.jsx', '.js'],
     },
 
     entry: {
-        app: ['./client'],
+        app: './client',
     },
-
     module: {
         rules: [{
-            test: /\.jsx?/,
+            test: /\.jsx?$/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: ['> 5% in KR', 'last 2 chrome versions'],
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react'
+                ],
                 plugins: ['@babel/plugin-proposal-class-properties'],
             },
         }],
     },
-
+    plugins: [
+        new webpack.LoaderOptionsPlugin({ debug: true }),
+    ],
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'app.js',
     },
-};
+}
