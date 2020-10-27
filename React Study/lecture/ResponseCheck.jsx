9b47ea3d0e5542d9e3fbe0a806988abc2cb93d4c
Jeseong Lee <wptjd6141@naver.com>
2020-10-27 02:54:35 -0700

    ResponseCheck class

--- React Study/lecture/ResponseCheck.jsx
+++ React Study/lecture/ResponseCheck.jsx
@@ -7,23 +7,62 @@ class ResponseCheck extends Component {
     result: [],
   };
 
-  onClickScreen = () => {};
+  timeout;
+  startTime;
+  endTime;
+
+  onClickScreen = () => {
+    const { state, message, result } = this.state;
+    if (state === "waiting") {
+      this.setState({
+        state: "ready",
+        message: "초록색이 되면 클릭하세요.",
+      });
+      this.timeout = setTimeout(() => {
+        this.setState({
+          state: "now",
+          message: "클릭",
+          result: [],
+        });
+        this.startTime = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000);
+    } else if (state === "ready") {
+      clearTimeout(this.timeout);
+      this.setState({
+        state: "waiting",
+        message: "초록색이 된 후에 클릭하세요.",
+      });
+    } else if (state === "now") {
+      this.endTime = new Date();
+      console.log(this.endTime - this.startTime);
+      this.setState((prevState) => {
+        return {
+          state: "waiting",
+          message: "클릭해서 시작해주세요.",
+          result: [...prevState.result, this.endTime - this.startTime],
+        };
+      });
+    }
+  };
+
+  renderAverage = () => {
+    const { result } = this.state;
+    return result.length === 0 ? null : (
+      <div>
+        평균 시간: {result.reduce((a, c) => a + c) / result.length}
+        ms
+      </div>
+    );
+  };
 
   render() {
+    const { state, message } = this.state;
     return (
       <>
-        <div
-          id="screen"
-          className={this.state.state}
-          onClick={this.onClickScreen}
-        >
-          {this.state.message}
+        <div id="screen" className={state} onClick={this.onClickScreen}>
+          {message}
         </div>
-        <div>
-          평균 시간:{" "}
-          {this.state.result.reduce((a, c) => a + c) / this.state.result.length}
-          ms
-        </div>
+        {this.renderAverage()}
       </>
     );
   }
