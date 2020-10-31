5f760dce54097da0810de247a07a1760445d57b4
Jeseong Lee <wptjd6141@naver.com>
2020-10-31 15:09:49 -0700

    Switch, props

---
+++ React Study/9. react-router/GameMatcher.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import NumberBaseball from "../3. 숫자야구/NumberBaseball";
+import RSP from "../5. 가위바위보/RSP";
+import Lotto from "../6. 로또추첨기/Lotto";
+
+class GameMatcher extends Component {
+  render() {
+    let urlSearchParams = new URLSearchParams(
+      this.props.location.search.slice(1)
+    );
+    console.log(urlSearchParams.get("hello"));
+    if (this.props.match.params.name === "number-baseball") {
+      return <NumberBaseball />;
+    } else if (this.props.match.params.name === "rock-scissors-paper") {
+      return <RSP />;
+    } else if (this.props.match.params.name === "lotto-generator") {
+      return <Lotto />;
+    }
+    return <div>일치하는 주소가 없음</div>;
+  }
+}
+
+export default GameMatcher;
--- React Study/9. react-router/Games.jsx
+++ React Study/9. react-router/Games.jsx
@@ -1,23 +1,46 @@
 import React from "react";
-import { BrowserRouter, HashRouter, Link, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  HashRouter,
+  Link,
+  Route,
+  Switch,
+} from "react-router-dom";
 import NumberBaseball from "../3. 숫자야구/NumberBaseball";
 import RSP from "../5. 가위바위보/RSP";
 import Lotto from "../6. 로또추첨기/Lotto";
+import GameMatcher from "./GameMatcher";
 
 const Games = () => {
   return (
     <BrowserRouter>
       <div>
-        <Link to="/number-baseball">숫자야구</Link>
+        <Link to="/game/number-baseball?query=10&hello=J3SUNG&bye=react">
+          숫자야구
+        </Link>
         &nbsp;
-        <Link to="/rock-scissors-paper">가위바위보</Link>
+        <Link to="/game/rock-scissors-paper">가위바위보</Link>
         &nbsp;
-        <Link to="/lotto-generator">로또추첨기</Link>
+        <Link to="/game/lotto-generator">로또추첨기</Link>
+        &nbsp;
+        <Link to="/game/index">게임 매쳐 </Link>
       </div>
       <div>
-        <Route path="/number-baseball" component={NumberBaseball}></Route>
-        <Route path="/rock-scissors-paper" component={RSP}></Route>
-        <Route path="/lotto-generator" component={Lotto}></Route>
+        <Switch>
+          <Route
+            path="/game/:name"
+            render={(props) => <GameMatcher {...props} />}
+          />
+          <Route
+            exact
+            path="/"
+            render={(props) => <GameMatcher {...props} />}
+          />
+          <Route
+            path="/game/:name"
+            render={(props) => <GameMatcher {...props} />}
+          />
+        </Switch>
       </div>
     </BrowserRouter>
   );
