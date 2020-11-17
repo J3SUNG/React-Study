5703bc3c6c6f0b167338af512bcc066617d2964b
Jeseong Lee <wptjd6141@naver.com>
2020-11-17 00:14:46 -0800

    react-router log

--- React Study/9. react-router/GameMatcher.jsx
+++ React Study/9. react-router/GameMatcher.jsx
@@ -5,6 +5,7 @@ import Lotto from "../6. 로또추첨기/Lotto";
 
 class GameMatcher extends Component {
   render() {
+    console.log(this.props);
     let urlSearchParams = new URLSearchParams(
       this.props.location.search.slice(1)
     );
